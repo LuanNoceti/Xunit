ff62829d1c8b3c40ec11e683cf150603e71ccfae
Luan Noceti Martins Das Neves <168584212+LuanNoceti@users.noreply.github.com>
2024-07-09 14:46:25 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Xunit
+{
+    internal static class Calculadora
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtrair(double x, double y) 
+        {
+            return x - y;
+        }
+
+        public static double Multiplicar(double x, double y) 
+        {
+            return x * y;
+        }
+
+        public static double Dividir(double x, double y) 
+        {
+            return x / y;
+        }
+    }
+}
---
+++ CalculadoraTeste.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+namespace Projeto_Xunit
+{
+    public class CalculadoraTeste
+    {
+        [Fact]
+        public void TestarSomar()
+        {
+            // Given
+
+            // When
+            double resultado = Calculadora.Somar(13, 10);
+
+            // Then
+            Assert.Equal(23, resultado);
+        }
+
+        [Fact]
+        public void TesteSubtrair()
+        {
+            // Given
+
+            // When
+            double resultado = Calculadora.Subtrair(13, 10);
+
+            // Then
+            Assert.Equal(3, resultado);
+        }
+
+        [Fact]
+        public void TesteMultiplicar()
+        {
+            // Given
+
+            // When
+            double resultado = Calculadora.Multiplicar(13, 10);
+
+            // Then
+            Assert.Equal(130, resultado);
+        }
+
+        [Fact]
+        public void TesteDividir()
+        {
+            // Given
+
+            // When
+            double resultado = Calculadora.Dividir(10, 5);
+
+            // Then
+            Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void TesteDividir2()
+        {
+            // Given
+
+            // When
+            double resultado = Calculadora.Dividir(10, 0);
+
+            // Then
+            Assert.Equal(Double.PositiveInfinity, resultado);
+        }
+    }
+}
